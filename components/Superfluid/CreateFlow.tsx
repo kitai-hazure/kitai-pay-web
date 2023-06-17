23a5c454387c790d0f3247ebdadd1680b97a8bcb
Akhilesh Manda <manda.akhilesh4@gmail.com>
2023-06-17 16:21:46 +0530

    :sparkles: Added getStreamsForUser (sender) to hook

--- components/Superfluid/CreateFlow.tsx
+++ components/Superfluid/CreateFlow.tsx
@@ -1,106 +1,57 @@
-import { Button } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 import React from "react";
 import { useNetwork } from "wagmi";
 import { getProvider, fetchSigner } from "@wagmi/core";
 import { Framework } from "@superfluid-finance/sdk-core";
 import { useSuperfluid } from "@/providers/SuperfluidProvider";
 
-const createNewFlow = async (recipientAddress: string, flowRate: number) => {
-  const provider = getProvider();
-  const signer = await fetchSigner();
-  console.log(signer);
-  //Initialize SDK - here we using polygon and getting provider from wagmi
-  const sf = await Framework.create({
-    chainId: 80001, //TODO: GET BASED ON CHAIN
-    provider,
-    resolverAddress: "0x8c54c83fbde3c59e59dd6e324531fb93d4f504d3",
-  });
-
-  const superSigner = await sf.createSigner({ signer: signer! });
-
-  console.log(await superSigner.getAddress());
-  //get addresses here - https://docs.superfluid.finance/superfluid/developers/networks
-  const fdaix = await sf.loadSuperToken(
-    "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f"
-  );
-
-  console.log(fdaix);
-
-  try {
-    const createFlowOperation = fdaix.createFlow({
-      sender: await superSigner.getAddress(),
-      receiver: recipientAddress,
-      flowRate: flowRate.toString(),
-      // userData?: string - can add description as well
-    });
-
-    console.log(createFlowOperation);
-    console.log("Creating your stream...");
-
-    const result = await createFlowOperation.exec(superSigner);
-    console.log(result);
-
-    console.log(
-      `Congrats - you've just created a money stream!
-      `
-    );
-  } catch (error) {
-    console.log(
-      "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
-    );
-    console.error(error);
-  }
-};
-
 const CreateFlow = () => {
   const [recipientAddress, setRecipientAddress] = React.useState<string>("");
   //0xc6B19e47Bbfb70D2c466E15F25506d2D4D1D5829
   const [flowRate, setFlowRate] = React.useState<number>(0);
-
-  const handleChangeRecipientAddress = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRecipientAddress(event.target.value);
-  };
+  const [loading, setLoading] = React.useState<boolean>(true);
 
   const handleChangeFlowRate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("called");
     setFlowRate(Number(event.target.value));
   };
 
-  const { sf, superSigner, fdaix } = useSuperfluid();
+  const { sf, superSigner, fdaix, createNewFlow, getAllFlowsForUser } =
+    useSuperfluid();
   React.useEffect(() => {
-    console.log("Running use effect");
-    console.log(sf);
-    console.log(superSigner);
-    console.log(fdaix);
     if (sf && superSigner && fdaix) {
       // Access sf, superSigner, and fdaix here
       console.log(sf);
       console.log(superSigner);
       console.log(fdaix);
+      setLoading(false);
     }
   }, [sf, superSigner, fdaix]);
 
-  return (
+  return loading ? (
+    <>Loading</>
+  ) : (
     <div className="flex h-screen flex-col items-center justify-center">
       <div>
         <h1>Let's Learn to create a stream</h1>
       </div>
-      <div className="pt-5">
-        <input
+      <div>
+        <Input
+          onChange={(e) => {
+            setRecipientAddress(e.target.value);
+          }}
+          placeholder="Enter recipient address"
           type="text"
-          className="rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
           value={recipientAddress}
-          onChange={handleChangeRecipientAddress}
-          placeholder="Enter Recipient Address"
         />
       </div>
-      <div className="p-5">
-        <input
+      <div>
+        <Input
           type="text"
-          className="rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
           value={flowRate.toString()}
-          onChange={handleChangeFlowRate}
+          onChange={(e) => {
+            setFlowRate(Number(e.target.value));
+          }}
           placeholder="Enter Flow Rate"
         />
       </div>
@@ -108,6 +59,7 @@ const CreateFlow = () => {
         <Button onClick={() => createNewFlow(recipientAddress, flowRate)}>
           Let's create a stream
         </Button>
+        <Button onClick={() => getAllFlowsForUser()}>Get Flows</Button>
       </div>
     </div>
   );
--- providers/SuperfluidProvider.tsx
+++ providers/SuperfluidProvider.tsx
@@ -1,18 +1,23 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { Framework, SuperToken } from "@superfluid-finance/sdk-core";
-import { ethers } from "ethers";
+import { Signer, ethers } from "ethers";
 import { getProvider, fetchSigner } from "@wagmi/core";
+import { useAccount, useSigner } from "wagmi";
 
 interface SuperfluidContextType {
   sf: Framework | null;
   superSigner: ethers.Signer | null;
   fdaix: SuperToken | null;
+  createNewFlow: Function;
+  getAllFlowsForUser: Function;
 }
 
 const SuperfluidContext = createContext<SuperfluidContextType>({
   sf: null,
   superSigner: null,
   fdaix: null,
+  createNewFlow: () => {},
+  getAllFlowsForUser: () => {},
 });
 
 export function useSuperfluid(): SuperfluidContextType {
@@ -27,38 +32,132 @@ export function SuperfluidProvider({
   const [sf, setSuperfluid] = useState<Framework | null>(null);
   const [superSigner, setSuperSigner] = useState<ethers.Signer | null>(null);
   const [fdaix, setFDAIX] = useState<SuperToken | null>(null);
-
+  const address = useAccount();
+  const { data: signer } = useSigner();
+  console.log(address);
   useEffect(() => {
     async function initializeSuperfluid() {
       const provider = getProvider();
-      const signer = await fetchSigner();
       const sfInstance = await Framework.create({
         chainId: 80001, // TODO: GET BASED ON CHAIN
         provider,
         resolverAddress: "0x8c54c83fbde3c59e59dd6e324531fb93d4f504d3",
       });
 
+      console.log("REACHED HERE");
+
+      console.log(signer);
       if (!signer) return;
-      const superSignerInstance = await sfInstance.createSigner({
-        signer: signer!,
+      const superSignerInstance = sfInstance.createSigner({
+        signer: signer as Signer,
       });
+
+      console.log("SUCCESSFUL");
+      console.log("Signer Address:", await superSignerInstance.getAddress());
 
       const fdaixInstance = await sfInstance.loadSuperToken(
         "0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f"
       );
+
+      console.log("GOT fdaix");
 
       setSuperfluid(sfInstance);
       setSuperSigner(superSignerInstance);
       setFDAIX(fdaixInstance);
     }
 
-    if (window.ethereum) {
+    if (address) {
+      console.log("RUNNING INIT SUPERFLUID");
       initializeSuperfluid();
     }
-  }, []);
+  }, [signer]);
+
+  //Function to Add new flow
+  const createNewFlow = async (
+    recipientAddress: string,
+    flowRate: number,
+    description?: string
+  ) => {
+    if (sf && superSigner && fdaix) {
+      try {
+        const createFlowOperation = fdaix.createFlow({
+          sender: await superSigner.getAddress(),
+          receiver: recipientAddress,
+          flowRate: flowRate.toString(),
+          userData: description,
+          //   userData?: string - can add description as well
+        });
+
+        console.log(createFlowOperation);
+        console.log("Creating your stream...");
+
+        const result = await createFlowOperation.exec(superSigner);
+        console.log(result);
+
+        console.log(
+          `Congrats - you've just created a money stream!
+        `
+        );
+      } catch (error) {
+        console.log(
+          "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
+        );
+        console.error(error);
+      }
+    } else {
+      throw new Error("Error initializing SDK");
+    }
+  };
+
+  //Function to get all flows from graphQL
+  const getAllFlowsForUser = async () => {
+    if (!(sf && superSigner && fdaix)) {
+      throw new Error("Error initializing SDK");
+    }
+    const superFluidAddress = await superSigner.getAddress();
+    console.log("Querying for address :", superFluidAddress.toLowerCase());
+    const data = await fetch(
+      //   "https://console.superfluid.finance/subgraph?_network=mumbai",
+      "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-mumbai",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `
+          query FetchStreamsFromAddress{
+            streams(where: {sender: "${superFluidAddress.toLowerCase()}"}) {
+              id
+              userData
+              currentFlowRate
+              receiver {
+                id
+                receivedTransferEvents {
+                  addresses
+                  transactionHash
+                  token
+                }
+              }
+            }
+          }
+      `,
+        }),
+        next: { revalidate: 10 },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        return data;
+      });
+
+    console.log("Data fetched from API : ", data);
+  };
 
   return (
-    <SuperfluidContext.Provider value={{ sf, superSigner, fdaix }}>
+    <SuperfluidContext.Provider
+      value={{ sf, superSigner, fdaix, createNewFlow, getAllFlowsForUser }}
+    >
       {children}
     </SuperfluidContext.Provider>
   );
